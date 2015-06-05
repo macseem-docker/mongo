1bc5b49bbaedb6c4acc288daabd0413ba3724b11
Maksim Dudkin <maksim.dudkin@mtechlab.net>
2015-06-05 21:23:45 +0300

    add noauth config; change scripts

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM debian:7.7
 MAINTAINER Max D. <lugamax@gmail.com>
 
-ADD  mongo_init.js /root/mongo_init.js
-ADD  mongo_init.sh /root/mongo_init.sh
 
 RUN  apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 \
      && echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" \
        | tee /etc/apt/sources.list.d/mongodb-org-3.0.list \
      && apt-get update \
-     && apt-get install -y -q adduser mongodb-org \
-     && /root/mongo_init.sh
+     && apt-get install -y -q procps adduser mongodb-org 
 
+ADD  mongo_init.js /root/mongo_init.js
+ADD  mongo_init.sh /root/mongo_init.sh
+ADD  mongod_noauth.conf /etc/mongod_noauth.conf
 ADD mongod.conf /etc/mongod.conf
+
+RUN /root/mongo_init.sh
 ADD init.sh /init.sh
--- mongo_init.sh
+++ mongo_init.sh
@@ -1,16 +1,44 @@
   FILE="/root/mongo_init.js"
   PIDFILE="/var/run/mongod.pid"
-  CONFIGFILE="/etc/mongod.conf"
+  NOAUTHCONFIGFILE="/etc/mongod_noauth.conf"
   MONGOUSER="mongodb"
-  DAEMONARGS="--noauth --config $CONFIGFILE"
+  DAEMON="/usr/bin/mongod"
   #service mongod start&
-  start-stop-daemon --background --start --quiet --pidfile $PIDFILE \
-                          --make-pidfile --chuid $MONGOUSER \
-                                                  --exec /usr/bin/mongod -- $DAEMONARGS &
+  rm /var/lib/mongodb/mongod.lock
+  start-stop-daemon -Sbvx /usr/bin/mongod -p /var/run/mongod.pid \
+     --make-pidfile --chuid mongodb -- --config /etc/mongod_noauth.conf
+#  start-stop-daemon --background --start --quiet \
+#                            --pidfile $PIDFILE \
+#                            --make-pidfile --chuid $MONGOUSER \
+#                            --exec /usr/bin/mongod \
+#                            -- --config $NOAUTHCONFIGFILE 
 
 #  start-stop-daemon --start --exec mongod --config $CONFIGFILE --noauth --make-pidfile $PIDFILE & 
+running_pid() {
+# Check if a given process pid's cmdline matches a given name
+    pid=$1
+    name=$2
+    [ -z "$pid" ] && return 1
+    [ ! -d /proc/$pid ] &&  return 1
+    cmd=`cat /proc/$pid/cmdline | tr "\000" "\n"|head -n 1 |cut -d : -f 1`
+    # Is this the expected server
+    [ "$cmd" != "$name" ] &&  return 1
+    return 0
+}
+
+running() {
+# Check if the process is running looking at /proc
+# (works for all users)
+
+    # No pidfile, probably no daemon present
+    [ ! -f "$PIDFILE" ] && return 1
+    pid=`cat $PIDFILE`
+    running_pid $pid $DAEMON || return 1
+    return 0
+}
+
     echo 'Waiting for mongo to come online'
-    while [ ! -f $PIDFILE ]; do
+    while [ ! running ] ; do
         sleep 1
     done
  
